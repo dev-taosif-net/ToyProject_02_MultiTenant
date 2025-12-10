7a8498dbd89c55ced2546b12b1ce21e5cceb9a05
Taosif <dev.taosif.net@gmail.com>
2025-12-10 23:30:49 +0600

    Add School entity and update project references

---
+++ Domain/Entities/School.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class School
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required DateOnly EstablishedDate { get; set; }
+}
