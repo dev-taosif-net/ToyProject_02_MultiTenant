30518066f4767bbca20d2fa5e5e48d61ad097c96
Taosif <dev.taosif.net@gmail.com>
2025-12-12 19:38:32 +0600

    Add identity models for user and role management

---
+++ Infrastructure/Identity/Models/ApplicationRoleClaim.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Models;
+
+public class ApplicationRoleClaim : IdentityRoleClaim<string>
+{
+    public string Description { get; set; } = string.Empty;
+    public string Group { get; set; } = string.Empty;
+}
---
+++ Infrastructure/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public bool IsActive { get; set; } = true;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime RefreshTokenExpiryTime { get; set; }
+    
+}
