36590b20092973c828dce1be481723703c2c128e
Taosif <dev.taosif.net@gmail.com>
2025-12-12 19:21:00 +0600

    Add infrastructure setup for multi-tenancy support

---
+++ Infrastructure/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using Finbuckle.MultiTenant.AspNetCore.Extensions;
+using Finbuckle.MultiTenant.EntityFrameworkCore.Extensions;
+using Finbuckle.MultiTenant.Extensions;
+using Infrastructure.Tenancy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class StartUp
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDbContext<TenantDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")))
+            .AddMultiTenant<CompanyTenantInfo>()
+            .WithHeaderStrategy(TenancyConstants.TenantIdName)
+            .WithClaimStrategy(TenancyConstants.TenantIdName)
+            .WithEFCoreStore<TenantDbContext, CompanyTenantInfo>()
+            .Services;
+
+    }
+}
