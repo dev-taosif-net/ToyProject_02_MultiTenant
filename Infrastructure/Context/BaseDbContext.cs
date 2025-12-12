452e826b44bd1abedd6d0653a4839ffe9e0c4191
Taosif <dev.taosif.net@gmail.com>
2025-12-12 20:18:14 +0600

    Add BaseDbContext for multi-tenant identity management

---
+++ Infrastructure/Context/BaseDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using Finbuckle.MultiTenant.Identity.EntityFrameworkCore;
+using Infrastructure.Identity.Models;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public abstract class BaseDbContext
+    : MultiTenantIdentityDbContext<
+        ApplicationUser,           // TUser
+        ApplicationRole,           // TRole
+        string,                    // TKey
+        IdentityUserClaim<string>, // TUserClaim
+        IdentityUserRole<string>,  // TUserRole
+        IdentityUserLogin<string>, // TUserLogin
+        ApplicationRoleClaim,      // TRoleClaim 
+        IdentityUserToken<string>, // TUserToken
+        IdentityUserPasskey<string> // TUserPasskey 
+    >
+{
+    protected BaseDbContext( IMultiTenantContextAccessor<CompanyTenantInfo> multiTenantContextAccessor, DbContextOptions options) : base(multiTenantContextAccessor, options)
+    {
+    }
+
+    // protected override void OnModelCreating(ModelBuilder builder)
+    // {
+    //     base.OnModelCreating(builder);
+    // }
+}
+
+
+
+
